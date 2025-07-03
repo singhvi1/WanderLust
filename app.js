8588db6cd81ada290b2c9e00a5236bbf704debfb
singhvi1 <singvi182003@gmail.com>
2025-07-03 09:27:39 +0530

    fixing /

--- app.js
+++ app.js
@@ -89,9 +89,9 @@ app.use("/listings",listingRouter);
 app.use("/listings/:id/reviews",reviewRouter);
 app.use("/",userRouter)
 
-// app.get("/", (req, res) => {
-//   res.send("Hi i am root");
-// });
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+}); 
 app.all("*", (req, res, next) => {
   next(new ExpressError(404, "page not found!"));
 });
